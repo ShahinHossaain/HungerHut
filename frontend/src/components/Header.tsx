c14651a0cce886f510aba7946d6a263e3ffa47e9
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-05-08 18:30:46 +0600

    main nab added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header: React.FC = () => {
   return (
@@ -13,6 +14,9 @@ const Header: React.FC = () => {
         </Link>
         <div className="md:hidden">
           <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
