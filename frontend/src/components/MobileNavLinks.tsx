1f9900ceb1d51be8b153b993ef9b40a37aaaf2b8
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-05-10 00:33:19 +0600

    signin signout functionalities added

---
+++ frontend/src/components/MobileNavLinks.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const MobileNavLinks: React.FC = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <>
+      <Link
+        to="/user-profile"
+        className="flex bg-white items-center font-bold hover:text-orange-500 px-3 py-2 rounded"
+      >
+        User Profile
+      </Link>
+
+      <Button
+        className="flex items-center px-3 font-bold hover:bg-gray-500 text-white bg-orange-500"
+        onClick={() => logout()}
+      >
+        Log Out
+      </Button>
+    </>
+  );
+};
+
+export default MobileNavLinks;
---
+++ frontend/src/components/UsernameMenu.tsx
@@ -0,0 +1,43 @@
+import { CircleUserRound } from "lucide-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+const UsernameMenu: React.FC = () => {
+  const { user, logout } = useAuth0();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+        <CircleUserRound className="text-orange-500 mr-2" />
+        {user?.email}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold hover:text-orange-500">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Button
+            onClick={() => logout()}
+            className="flex flex-1 font-bold bg-orange-500 text-white hover:bg-orange-600"
+          >
+            Log Out
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UsernameMenu;
