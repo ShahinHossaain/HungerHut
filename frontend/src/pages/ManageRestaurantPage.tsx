c657f6fa615b40fffe108a5458629cb283a11894
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-05-26 10:09:57 +0600

    Manage Restaurant details cuisines, menu and image part is added.

---
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,7 @@
+import ManageRestaurantForm from "../forms/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage: React.FC = () => {
+  return <ManageRestaurantForm />;
+};
+
+export default ManageRestaurantPage;
